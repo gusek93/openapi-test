0f7c8e47615dc4ca4a08ce57bfd38feaa8310f0d
Dayong Hyun <46876637+gusek93@users.noreply.github.com>
2022-05-30 18:12:40 +0900

    rest-Assured add, curd test

---
+++ src/test/java/com/example/openapitest/member/RestAssuredTest.java
@@ -0,0 +1,61 @@
+package com.example.openapitest.member;
+
+import com.example.openapitest.member.domain.Member;
+import io.cucumber.java.hu.Ha;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestAssuredTest {
+
+    @Test
+    void getMember() {
+        given().
+            when().get("/member").then().log().all();
+
+    }
+
+    @Test
+    void createMember() {
+        Map<String, Object> requestData = new HashMap<>();
+        requestData.put("name", "testcase1");
+
+        RestAssured.given().contentType("application/json")
+                .body(requestData).log().all()
+                .when().post("/member")
+                .then()
+                    .assertThat().body("name", equalTo("testcase1")).log().all();
+
+    }
+
+    @Test
+    public void updateMember() {
+        Map<String, Object> requestData = new HashMap<>();
+        requestData.put("id", 72);
+        requestData.put("name", "fuckkingBDD");
+
+        RestAssured.given()
+            .contentType("application/json")
+            .pathParam("id",72)
+            .body(requestData).log().all()
+            .when().put("/member/{id}")
+            .then().statusCode(200)
+            .assertThat().body("id",equalTo(72))
+            .assertThat().body("name", equalTo("fuckkingBDD")).log().all();
+
+    }
+
+    @Test
+    public void deleteMember() {
+        RestAssured.given()
+            .pathParam("id",71).log().all()
+            .when().delete("/member/{id}")
+            .then().statusCode(200)
+            .log().all();
+    }
+}
